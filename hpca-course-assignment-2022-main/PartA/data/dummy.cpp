ba9136fc557ec3ef30cc05590c793f825a016006
pranav <pranavgajera1998@gmail.com>
2022-11-30 20:19:17 +0530

    Added GPU Thread header

--- hpca-course-assignment-2022-main/PartA/data/dummy.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<endl<<"pranavgajera@Pranavs-MacBook-Air PartA % make run \n./rmm data/input_16.in\nInput matrix of size 16\nReference execution time: 0.021 ms\nSingle thread execution time: 0.024 ms\nMulti-threaded execution time: 0.19 ms\n./rmm data/input_4096.in\nInput matrix of size 4096\nReference execution time: 308119 ms\nSingle thread execution time: 10971.6 ms\nMulti-threaded execution time: 3189.1 ms"<<endl;
-return 0;
-}
