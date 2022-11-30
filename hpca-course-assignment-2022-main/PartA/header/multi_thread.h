9f7f7b48469bcdc15df5fbecd4ca1472e0f5c34b
Puspak Sahu <puspak@Puspak-Sahus-MacBook-Air.local>
2022-11-30 23:01:10 +0530

    2nd change

--- hpca-course-assignment-2022-main/PartA/header/multi_thread.h
+++ hpca-course-assignment-2022-main/PartA/header/multi_thread.h
@@ -1,5 +1,26 @@
 #include <pthread.h>
 
+// not using this just for checking 
+void *rmm_mult_1(void* arg)
+{
+    int *data = (int *)arg;
+    int k = 0, i = 0;
+    int N = data[0];
+    for (i = 1; i <= N; i++){
+        k += data[i]*data[i+(2*N)];
+        k += data[i]*data[i+(3*N)];
+        k += data[i+N]*data[i+(2*N)];
+        k += data[i+N]*data[i+(3*N)];
+    } 
+    int *p = (int*)malloc(sizeof(int));
+    *p = k;
+
+    //Used to terminate a thread and the return value is passed as a pointer
+    pthread_exit(p);
+}
+
+
+//function that is used
 void* rmm_mult(void* arg){
     int *data = (int *)arg;
     int k=0,i=0;
@@ -17,8 +38,9 @@ void* rmm_mult(void* arg){
     pthread_exit((void*) result_t);
 }
 
-void multiThread(int N, int *matA, int *matB, int *output){
-
+// Fill in this function
+void multiThread(int N, int *matA, int *matB, int *output)
+{
     int max = 4;
     pthread_t *threads;
     threads = (pthread_t*)malloc(max*sizeof(pthread_t));
@@ -26,10 +48,12 @@ void multiThread(int N, int *matA, int *matB, int *output){
     int count = 0;
     int* data = NULL;
     for (int i = 0; i < max; i++){
+            //storing row and column elements in data
             data = (int *)malloc(((N*N)+1)*sizeof(int));
             data[0] = N;
             int m=0;
-            int a=0,b=0;
+            int a=0;
+            int b=0;
             if(i==0) {a=0;b=0;}
             else if(i==1) {a=1;b=0;}
             else if(i==2) {a=0;b=1;}
@@ -47,24 +71,24 @@ void multiThread(int N, int *matA, int *matB, int *output){
                 }
             }
             int error_code = pthread_create(&threads[count++],NULL,rmm_mult, data);
-            if(error_code){
-                cout<<"Thread creation failure with error code ::: "<< error_code <<endl;
-                exit(0);
+            if(error_code != 0){
+                cout<<"Thread Creation Failure with error code ::: "<< error_code <<endl;
             }
     }
     int* result[max];
     for (int i = 0; i < max; i++){
+      //Joining all threads and collecting return value
         int *result_t;
         int error_code = pthread_join(threads[i],(void**)&result_t);
-        if(error_code){
-            cout<<"Thread join failure with error code ::: "<<error_code<<endl;
-            exit(0);
+        if(error_code!=0){
+            cout<<"Thread Join failure with error Code ::: "<<error_code<<endl;
         }
         result[i] = (int *) malloc(((N*N)/4)*sizeof(int));
         for(int j=0;j<(N*N)/4;j++){
             result[i][j]=result_t[j];
         }
         free(result_t);
+        
     }
     for(int i=0;i<max;i++){
         for(int j=0;j<((N*N)/4);j++){
@@ -72,4 +96,3 @@ void multiThread(int N, int *matA, int *matB, int *output){
         }
     }
 }
-
--- hpca-course-assignment-2022-main/PartA/header/single_thread.h
+++ hpca-course-assignment-2022-main/PartA/header/single_thread.h
@@ -1,13 +1,13 @@
 typedef int vec __attribute__(( vector_size(32) ));
 
 //dynamically change the vector size
-
 #define vecsize 8
 vec* memalloc(int n) {
-    vec* ptr = (vec*) aligned_alloc(32, 32 * n);
+    int size=32*n;
+    vec* p = (vec*) aligned_alloc(32, size);
     //set to 0
-    memset(ptr, 0, 32 * n);
-    return ptr;
+    memset(p, 0, size);
+    return p;
 }
 
 
@@ -18,16 +18,20 @@ void singleThread(int n,int *_a, int *_b,int *c) {
     vec *b = memalloc(totalvec);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            a[i * vin + j / vecsize][j %vecsize] = _a[i * n + j];
-            b[i * vin + j / vecsize][j % vecsize] = _b[j * n + i]; 
+
+            //I is the index of I th vector
+            //J is the index of vector element
+            int I= i * vin + j / vecsize;
+            int J =j % vecsize;
+            a[I][J] = _a[i * n + j];
+            b[I][J] = _b[j * n + i]; 
         }
     }
 
-
+    int start;
     for (int i = 0; i < n; i+=2) {
         for (int j = 0; j < n; j+=2) {
             vec s; 
-
             //initialise to zero
             //for no garbage value
             for (int ll=0;ll<vecsize;++ll)
@@ -36,6 +40,7 @@ void singleThread(int n,int *_a, int *_b,int *c) {
             }
             int k=0;
             //s is a vector
+            //multiply internal vectors
             while(k<vin){
                 s += a[i * vin + k] * b[j * vin + k];
                 s += a[(i+1) * vin + k] * b[j * vin + k];
@@ -45,7 +50,7 @@ void singleThread(int n,int *_a, int *_b,int *c) {
             }
 
             //update the s and sum it vertically
-            for (int k = 0; k < vecsize; k++)
+            for (int k = vecsize-1 ; k >=0; k--)
                 c[(i>>1) * (n>>1) + (j>>1)] += s[k];
         }
     }
